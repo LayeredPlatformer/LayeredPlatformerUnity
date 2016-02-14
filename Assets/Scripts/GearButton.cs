1636a20651eec9784d7a3998eb1b50f7ee0dd647
fudge22 <fudge22it@gmail.com>
2016-02-13 23:48:37 -0700

    Gear Button update
    Allows for the creation of buttons that are activated when a gear
    touches them.

---
+++ Assets/Scripts/GearButton.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GearButton : MonoBehaviour {
+
+    public string Gear = "Gear";
+
+	// Use this for initialization
+	void Start () 
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+	
+	}
+
+    void OnTriggerEnter(Collider collider)
+    {
+        if (collider.gameObject.tag == Gear)
+        {
+            //The large gear entered the box. Do something.
+            Debug.Log("The gear hit the box.");
+        }
+    }
+}
