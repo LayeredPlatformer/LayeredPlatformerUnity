340903651c8856cb67c9e7d1833bddc6d2b99f96
Chris Ward <marcgyven@gmail.com>
2016-02-20 15:09:16 -0700

    Settings screen added, Title screen button to settings works.
    Also contains a trial build of the project, which I think I needed to
    test the connection between scenes.

---
+++ Assets/Scripts/LoadScene.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadScene : MonoBehaviour {
+
+    public string Destination;
+
+    public void Load()
+    {
+        Application.LoadLevel(Destination);
+    }
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
