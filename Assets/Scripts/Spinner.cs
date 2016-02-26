707554c338ebb31cc2c603f2f342f68f5e56a6f1
Jameson Saunders <jammer@me.com>
2016-02-25 17:38:42 -0700

    trap door and spinner

---
+++ Assets/Scripts/Spinner.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spinner : MonoBehaviour {
+
+	public float rotSpeed = 1;
+
+	private Vector3 _rotation = Vector3.zero;
+
+	void Start ()
+	{
+	
+	}
+	
+	void Update ()
+	{
+		Rotate();
+	}
+
+	void Rotate()
+	{
+		_rotation = new Vector3(0,0,_rotation.z-rotSpeed*Time.fixedDeltaTime*Utility.getFPS());
+		transform.localRotation = Quaternion.Euler(_rotation);
+	}
+}
---
+++ Assets/Scripts/TrapDoorTrigger.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TrapDoorTrigger : MonoBehaviour
+{
+	public Rigidbody trapDoorRigidBody;
+
+	private float _releaseTime = .5f;
+
+	void OnTriggerEnter(Collider collider)
+	{
+		if (!collider.GetComponent<PlayerController>())
+			return;
+		Invoke("ReleaseDoor", _releaseTime);
+	}
+
+	void ReleaseDoor()
+	{
+		trapDoorRigidBody.isKinematic = false;
+	}
+}
