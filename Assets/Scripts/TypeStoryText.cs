2bc1bd28eb167fb86ecf34f350bd4eccc10207fd
Chris Ward <marcgyven@gmail.com>
2016-03-07 23:50:09 -0700

    Tinkering with TypeStoryText script, trying to get it to read from file instead of hard coding the text.

--- Assets/Scripts/TypeStoryText.cs
+++ Assets/Scripts/TypeStoryText.cs
@@ -10,7 +10,7 @@ public class TypeStoryText : MonoBehaviour {
     public Text text;
     public double typeSpeed = .25;
     private double counter;
-    private string fileText= "The Watchers. A group of beings who for untold ages have guarded the sanctity of time. Observing from outside of time, they monitor the proper flow of time and remedy any anomalies that they detect. \n\nThe least of the Watchers is called Apprentice. While the others primarily monitor the flow of time, Apprentice is the one sent to handle most of the anomalies that they find. \n\nBut one day, as he was observing the Earth, he found an anomaly that the others had not yet told him about.He reported this to the chief Watcher, Aion.But Aion replied that it was nothing to worry about yet, and forbade Apprentice from going to Earth to fix it.\n\nApprentice couldn't just let it go, however, and when the anomaly suddenly multiplied, he knew that he had to do something about it. He set out for Earth, defying his master's orders to do what he knew was right.Thus, his adventure began.";
+    private string fileText = "The Watchers. A group of beings who for untold ages have guarded the sanctity of time. Observing from outside of time, they monitor the proper flow of time and remedy any anomalies that they detect. \n\nThe least of the Watchers is called Apprentice. While the others primarily monitor the flow of time, Apprentice is the one sent to handle most of the anomalies that they find. \n\nBut one day, as he was observing the Earth, he found an anomaly that the others had not yet told him about.He reported this to the chief Watcher, Aion.But Aion replied that it was nothing to worry about yet, and forbade Apprentice from going to Earth to fix it.\n\nApprentice couldn't just let it go, however, and when the anomaly suddenly multiplied, he knew that he had to do something about it. He set out for Earth, defying his master's orders to do what he knew was right.Thus, his adventure began.";
 	
     public void awake()
     {
@@ -21,6 +21,7 @@ public class TypeStoryText : MonoBehaviour {
 
     public string content()
     {
+        Console.WriteLine("Trying to read file.");
         string output = "";
         try
         {
@@ -39,10 +40,15 @@ public class TypeStoryText : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+        if (fileText.Equals(""))
+            fileText = content();
         if (counter < fileText.Length)
+        {
             text.text = fileText.Substring(0, (int)counter);
+            counter += typeSpeed;
+        }
         else text.text = fileText;
-        counter+=typeSpeed;
-        Console.WriteLine("updating");
+        
+        //Console.WriteLine("updating");
 	}
 }
