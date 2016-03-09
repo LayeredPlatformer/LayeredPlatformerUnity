f72bff22d7cd9ea97ede6c9f5c9f6349261bba97
Dan M <the.code.writer@gmail.com>
2016-03-08 21:23:28 -0700

    Added checkpoint prefab and basic script

---
+++ Assets/Scripts/CheckpointTrigger.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CheckpointTrigger : MonoBehaviour
+{
+	// Use this for initialization
+	void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Checkpoint encountered! "+ other.name);
+    }
+}
