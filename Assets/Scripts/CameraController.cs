abe9b26667d6a9d13a30ebb59aae4c01725a5309
Dan M <the.code.writer@gmail.com>
2016-03-09 18:34:16 -0700

    Camera pan now works in z space as well as x/y and seems to no longer need the constant

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -25,19 +25,17 @@ public class CameraController : MonoBehaviour
 			transform.position = new Vector3(Player.transform.position.x, Player.transform.position.y, Player.transform.position.z - DistanceFromPlayer);
 		else
 		{
-			float oldZ = transform.position.z;
 			Vector3 moved = Vector3.MoveTowards(transform.position, _panPos, _panRate*Time.deltaTime);
-			transform.position = new Vector3(moved.x, moved.y, oldZ);
+			transform.position = new Vector3(moved.x, moved.y, moved.z);
 		}
 	}
 
 	public void pan(Vector3 pos, float seconds, Action callback = null)
 	{
 		_panning = true;
-		_panPos = pos;
+		_panPos = new Vector3(pos.x, pos.y, pos.z - DistanceFromPlayer);
 		// R = D/T
-		_panRate = Vector3.Distance(pos, transform.position) * 3/(seconds);
-		//TODO: idk why my equations for pan rate don't work: 3 is a magic number that fixes it
+		_panRate = Vector3.Distance(_panPos, transform.position)/(seconds);
 		Invoke("stopPan", seconds);
         InvokeAction.Invoke(callback, seconds);
     }
