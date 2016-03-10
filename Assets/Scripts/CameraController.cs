703e2ce4676532a9cef9f6cd1f9d106a83b822e9
Dan M <the.code.writer@gmail.com>
2016-03-09 18:21:28 -0700

    Replace magic number

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -36,7 +36,7 @@ public class CameraController : MonoBehaviour
 		_panning = true;
 		_panPos = pos;
 		// R = D/T
-		_panRate = Vector3.Distance(pos, transform.position)/(seconds);
+		_panRate = Vector3.Distance(pos, transform.position) * 3/(seconds);
 		//TODO: idk why my equations for pan rate don't work: 3 is a magic number that fixes it
 		Invoke("stopPan", seconds);
         InvokeAction.Invoke(callback, seconds);
