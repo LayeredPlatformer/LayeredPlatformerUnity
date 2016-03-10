7a2bdd6e3a10a429fe93f7b1e6f3b72077eddc87
Chris Ward <marcgyven@gmail.com>
2016-03-09 23:01:32 -0700

    Settings screen mostly functional. Audio and Controls subscreens implemented. Back button currently returns to title screen, not yet dynamic.
    Update to Title screen: Load game now points to Parallax3D, New game points to Story screen.
    
    Update to Story screen: Skip button now points to Parallax3D. Don't have tapping screen once the typing is done go there yet.

---
+++ Assets/Scripts/SettingsManager.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class SettingsManager : MonoBehaviour {
+
+    public GameObject[] toDisable;
+    public GameObject returnDestination;
+    public bool rightHandMode;
+    public Text modeText;
+    public Text modeButtonText;
+
+	// Use this for initialization
+	void Start () {
+        foreach (GameObject o in toDisable)
+        {
+            o.SetActive(false);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+
+    public void ToggleControlMode()
+    {
+        if(rightHandMode)
+        {
+            rightHandMode = false;
+            modeText.text = "Current mode: Left Handed";
+            modeButtonText.text = "Switch to Right Handed";
+        }
+        else
+        {
+            rightHandMode = true;
+            modeText.text = "Current mode: Right Handed";
+            modeButtonText.text = "Switch to Left Handed";
+        }
+    }
+}
