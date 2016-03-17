cc6e609684a56223a1ccc0768b8e934c6ea670cb
Jameson Saunders <jammer@me.com>
2016-03-16 18:59:29 -0600

    force on shooter enemy

--- Assets/Scripts/RangedEnemy.cs
+++ Assets/Scripts/RangedEnemy.cs
@@ -4,48 +4,52 @@ using System.Collections;
 public class RangedEnemy : MonoBehaviour 
 {
 	// TODO: update to follow c# conventions
-    public float speed = .01f;
-    public float Range = 5f;
-    public float maxRange = 10f;
-    public float xOffset = 1.5f;
-    public float yOffset = .5f;
-    private Transform Player;
-    private SpriteRenderer SpriteRenderer;
-    private Rigidbody rb;
+    public float Speed = .01f;
+    public float MinRange = 5f;
+    public float MaxRange = 10f;
+    public float XOffset = 1.5f;
+    public float YOffset = .5f;
+
+    private Transform _player;
+    private SpriteRenderer _spriteRenderer;
+    private Rigidbody _rb;
 
 	// Use this for initialization
 	void Start () 
     {
-        rb = gameObject.GetComponent<Rigidbody>();
-        Player = GameObject.FindGameObjectWithTag("Player").transform;
-        SpriteRenderer = GetComponent<SpriteRenderer>();
+        _rb = gameObject.GetComponent<Rigidbody>();
+        _player = GameObject.FindGameObjectWithTag("Player").transform;
+        _spriteRenderer = GetComponent<SpriteRenderer>();
 	}
 	
 	// Update is called once per frame
 	void Update () 
     {
-        Vector3 tdir = Player.position - transform.position;
-        bool sameLayers = GetComponent<LayeredController>().Layer.Equals(Player.GetComponent<LayeredController>().Layer);
+        Vector3 tdir = _player.position - transform.position;
+        bool sameLayers = GetComponent<LayeredController>().Layer.Equals(_player.GetComponent<LayeredController>().Layer);
         tdir.y = 0;
 
 		//TODO: update dispenser.Dispensing depending on distance to player
         if (sameLayers)
         {
-        	SpriteRenderer.flipX = tdir.x <= 0;
-            if (tdir.magnitude > Range && tdir.magnitude < maxRange)
-            {
-                tdir = tdir.normalized * speed;
-                transform.position += tdir;
-            }
+        	_spriteRenderer.flipX = tdir.x <= 0;
+
+            if (tdir.magnitude > MinRange && tdir.magnitude < MaxRange)
+			{
+				if (_spriteRenderer.flipX)
+					_rb.AddForce(-transform.right*Speed);
+				else
+					_rb.AddForce(transform.right*Speed);
+			}
 
 			Transform child = transform.GetChild(0);
             if (child.tag == "ShooterAIDispenser")
             {
-                if (SpriteRenderer.flipX == true)
-                    xOffset = xOffset * -1;
+                if (_spriteRenderer.flipX == true)
+                    XOffset = XOffset * -1;
 
-                Vector3 dispenserPos = new Vector3(transform.position.x + xOffset,
-					transform.position.y + yOffset, transform.position.z);
+                Vector3 dispenserPos = new Vector3(transform.position.x + XOffset,
+					transform.position.y + YOffset, transform.position.z);
                 child.position = dispenserPos;
             }
         }
