9daa708b1da9163c5535f80cd629ad2e0fc5215e
Jameson Saunders <jammer@me.com>
2016-03-31 16:02:36 -0600

    fully functional shadowBlink and layerJump buttons with stub images

--- Assets/Scripts/GameUI.cs
+++ Assets/Scripts/GameUI.cs
@@ -6,16 +6,33 @@ public class GameUI : MonoBehaviour
 	PlayerController _player;
 	Targetable _playerTargetable;
 	Texture _oilScreen;
+	Texture _shadowBlinkIcon;
+	Texture _layerJumpIcon;
 	const float _buttonWidth = 64f;
 	const float _buttonHeight = _buttonWidth;
 	Rect _shadowBlinkRect = new Rect(Screen.width-_buttonWidth, Screen.height-_buttonHeight, _buttonWidth, _buttonHeight);
 	Rect _layerJumpRect = new Rect(Screen.width-_buttonWidth*2, Screen.height-_buttonHeight, _buttonWidth, _buttonHeight);
+	bool _shadowBlinkPressed = false;
+	bool _layerJumpPressed = false;
+	float _buttonPressedDuration = .1f;
 
 	void Start ()
 	{
 		_player = GameObject.Find ("Player").GetComponent<PlayerController>();
 		_playerTargetable = _player.GetComponent<Targetable> ();
 		_oilScreen = (Texture)Resources.Load ("OilScreen");
+		_shadowBlinkIcon = (Texture)Resources.Load ("shadowBlinkIcon");
+		_layerJumpIcon = (Texture)Resources.Load ("layerJumpIcon");
+	}
+
+	void resetLayerJumpPressed()
+	{
+		_layerJumpPressed = false;
+	}
+
+	void resetShadowBlinkPressed()
+	{
+		_shadowBlinkPressed = false;
 	}
 
 	void OnGUI ()
@@ -26,11 +43,33 @@ public class GameUI : MonoBehaviour
 		GUI.color = temp;
 		GUI.DrawTexture (new Rect (0, 0, Screen.width, Screen.height), _oilScreen);
 
-		GUI.color = Color.white;
 		// player controls
 		if (GUI.Button(_shadowBlinkRect, ""))
+		{
 			_player.InitiateShadowBlink();
+			_shadowBlinkPressed = true;
+			CancelInvoke("resetShadowBlinkPressed");
+			Invoke("resetShadowBlinkPressed", _buttonPressedDuration);
+		}
 		if (GUI.Button(_layerJumpRect, ""))
+		{
 			_player.InitiateLayerJump();
+			_layerJumpPressed = true;
+			CancelInvoke("resetLayerJumpPressed");
+			Invoke("resetLayerJumpPressed", _buttonPressedDuration);
+		}
+
+
+		if (_shadowBlinkPressed)
+			GUI.color = Color.gray;
+		else
+			GUI.color = Color.white;
+		GUI.DrawTexture(_shadowBlinkRect, _shadowBlinkIcon);
+			
+		if (_layerJumpPressed)
+			GUI.color = Color.gray;
+		else
+			GUI.color = Color.white;
+		GUI.DrawTexture(_layerJumpRect, _layerJumpIcon);
 	}
 }
