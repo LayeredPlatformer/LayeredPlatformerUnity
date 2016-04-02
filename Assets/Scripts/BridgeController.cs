086ba1c2412ab19b72abe06c7b761855b493ba89
Chris Ward <marcgyven@gmail.com>
2016-04-02 17:19:49 -0600

    Developed a bridge prefab from the dead tree and the trigger box. Can be seen and tried in my playground.
    Also fixed an oddity with my short ice platform.

---
+++ Assets/Scripts/BridgeController.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BridgeController : Triggerable
+{
+
+    public float RotateAngle = 45f;
+    public float RotateRate = .1f;
+    public Utility.Directions RotateDirection;
+
+    private bool _retracting = false;
+    private float _distanceRetracted = 0;
+
+    void Update()
+    {
+        if (_retracting && _distanceRetracted < RotateAngle)
+        {
+            _distanceRetracted += RotateRate;
+            Quaternion rot = transform.rotation;
+            switch (RotateDirection)
+            {
+                case Utility.Directions.left:
+                    rot.z += RotateRate;
+                    break;
+                case Utility.Directions.right:
+                    rot.z -= RotateRate;
+                    break;
+            }
+            transform.rotation = rot;
+        }
+    }
+
+    public override void Trigger()
+    {
+        _retracting = true;
+    }
+
+}
